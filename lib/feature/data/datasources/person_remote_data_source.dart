6760f7ced4fb9072f0b3c99393277f528d91542a
melniknikolay <unclenikoboss@gmail.com>
2021-10-01 11:59:20 +0300

    add Remote Data Source

---
+++ lib/feature/data/datasources/person_remote_data_source.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:rick_and_morty/core/error/exception.dart';
+
+import 'package:rick_and_morty/feature/data/models/person_model.dart';
+
+abstract class PersonRemoteDataSource {
+  //Call the 'https://rickandmortyapi.com/api/character/?page=1' endpoint.
+  //
+  //Throw a [ServerException] for all error codes.
+  Future<List<PersonModel>> getAllPersons(int page);
+
+  //Call the 'https://rickandmortyapi.com/api/character/?name=name' endpoint.
+  //
+  //Throw a [ServerException] for all error codes.
+  Future<List<PersonModel>> searchPerson(String query);
+}
+
+class PersonRemoteDataSourceImpl implements PersonRemoteDataSource {
+  final http.Client client;
+  PersonRemoteDataSourceImpl({required this.client});
+
+  @override
+  Future<List<PersonModel>> getAllPersons(int page) => _getPersonFromUrl(
+      'https://rickandmortyapi.com/api/character/?page=$page');
+
+  @override
+  Future<List<PersonModel>> searchPerson(String query) => _getPersonFromUrl(
+      'https://rickandmortyapi.com/api/character/?name=$query');
+
+  Future<List<PersonModel>> _getPersonFromUrl(String url) async {
+    print(url);
+    final response = await client
+        .get(Uri.parse(url), headers: {'Content-Type': 'aplication/json'});
+    if (response.statusCode == 200) {
+      final persons = json.decode(response.body);
+      return (persons['results'] as List)
+          .map((person) => PersonModel.fromJson(person))
+          .toList();
+    } else {
+      throw ServerException();
+    }
+  }
+}
