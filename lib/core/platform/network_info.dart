1e819d13c197c9cc9a5a65da6041135bdd31b2c0
melniknikolay <unclenikoboss@gmail.com>
2021-10-01 13:11:49 +0300

    implemented data repository

---
+++ lib/core/platform/network_info.dart
@@ -0,0 +1,16 @@
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+
+/// Check internet connection on device
+
+abstract class NetworkInfo {
+  Future<bool> get isConnected;
+}
+
+class NetworkInfoImpl implements NetworkInfo {
+  final InternetConnectionChecker connectionChecker;
+
+  NetworkInfoImpl(this.connectionChecker);
+
+  @override
+  Future<bool> get isConnected => connectionChecker.hasConnection;
+}
